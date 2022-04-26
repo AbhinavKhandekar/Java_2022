4e956caaa697b897b8974e5e57c6b4e3f8edf5ff
Abhinav Khandekar <100548596+AbhinavKhandekar@users.noreply.github.com>
2022-04-26 20:20:39 +0530

    Data Types
    integer.floating,boolean,char etcs

---
+++ DataType.java
@@ -0,0 +1,49 @@
+package JavaPractice;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		//Integer Data Type
+		//1.byte
+		byte num=100;
+		System.out.println(num);
+		
+		//2.short
+		short mynum=5000;
+		System.out.println(mynum);
+		
+		//3.long
+		long myNum=1000000;
+		System.out.println(myNum);
+		
+		//Floating Data Type
+		//float
+		float a=5.36f;
+		System.out.println(a);
+		
+		//Double
+		double b=20.256d;
+		System.out.println(b);
+		
+		//Scientific Number
+		float c=36e25f;
+		double d=44e42d;
+		System.out.println(c);
+		System.out.println(d);
+		
+		//BooleanData Type
+		//Boolean
+		boolean isJavafun=true;
+		boolean isBurgerTaste=false;
+		System.out.println(isJavafun);
+		System.out.println(isBurgerTaste);
+		
+		//StringDataType
+		String myName="Abhinav Khandekar";
+		System.out.println(myName);
+		
+		
+
+	}
+
+}
