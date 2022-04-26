17ff55d2446061fc25d82a7d01fea04ce1abda22
Abhinav Khandekar <100548596+AbhinavKhandekar@users.noreply.github.com>
2022-04-26 20:23:11 +0530

    Arithmetic operators
    Arithmetic operators - addition,subtraction,multiplication,division,modulus,inc and dec

---
+++ Arithmetic_Operators.java
@@ -0,0 +1,34 @@
+package JavaPractice;
+
+public class Arithmetic_Operators {
+
+	public static void main(String[] args) {
+		// Arithmetic Operators
+		//1.Addition of 2 numbers
+		int a=20;
+		int b=30;
+		int sum=a+b;
+		System.out.println(sum);
+		//2.Subtraction of 2 numbers
+		int subtraction=a-b;
+		System.out.println(subtraction);
+		//multiplictaion of 2 numbers
+		int mul=a*b;
+		System.out.println(mul);
+		//division of 2 numbers
+		int div=a/b;
+		System.out.println(div);
+		//modulus of 2 numbers
+		int mod=a%b;
+		System.out.println(mod);
+		//Increment of number
+		int inc=++a;
+		System.out.println(inc);
+		//Decrement of number
+		int dec=--b;
+		System.out.println(dec);
+		
+
+	}
+
+}
