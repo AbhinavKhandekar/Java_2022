cd1351aad5ee60aa9756b4bdf17e0c850abba59c
Abhinav Khandekar <100548596+AbhinavKhandekar@users.noreply.github.com>
2022-04-26 20:21:52 +0530

    Data Casting
    Widening Casting and Narrowing Casting

---
+++ Casting.java
@@ -0,0 +1,21 @@
+package JavaPractice;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		//Widening Casting
+		int num=20;
+		double num1=num;
+		System.out.println(num);
+		System.out.println(num1);
+		
+		//Narrowing Casting
+		double mynum=25000;
+		int mynum1=(int) mynum;
+		System.out.println(mynum);
+		System.out.println(mynum1);
+		
+
+	}
+
+}
