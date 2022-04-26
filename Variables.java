a08ca9edb658609663719227cc858fd7848e13f5
Abhinav Khandekar <100548596+AbhinavKhandekar@users.noreply.github.com>
2022-04-26 19:08:44 +0530

    Variables
    String,Integer,Float,Boolean etc

---
+++ Variables.java
@@ -0,0 +1,30 @@
+package JavaPractice;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		//string
+		String name="Abhinav";
+		System.out.println(name);
+		
+		//Integer
+		int a=20;
+		System.out.println(a);
+		
+		//Float
+		Float num=5.20f;
+		System.out.println(num);
+		
+		//Char
+		char myletter='A';
+		System.out.println(myletter);
+		
+		//Boolean
+		boolean myBool=true;
+		System.out.println(myBool);
+		
+	
+
+	}
+
+}
