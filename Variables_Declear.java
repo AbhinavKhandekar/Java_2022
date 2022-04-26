03ae78d35555cefcb4a929b09bfff2f9df77b88d
Abhinav Khandekar <100548596+AbhinavKhandekar@users.noreply.github.com>
2022-04-26 20:24:34 +0530

    Variables_Declearation

---
+++ Variables_Declear.java
@@ -0,0 +1,14 @@
+package JavaPractice;
+
+public class Variables_Declear {
+
+	public static void main(String[] args) {
+		//Variables Decleration
+		int a=20,b=30,c=50;
+		System.out.println(a+b+c);
+		
+		
+
+	}
+
+}
